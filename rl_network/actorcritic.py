a408d2aa7045d4b79cac1cdc616f91348dc7fbb2
AnnikLINClab <annikcarson@gmail.com>
2018-09-18 15:13:32 -0400

    updating to work w conv nets

--- rl_network/actorcritic.py
+++ rl_network/actorcritic.py
@@ -34,7 +34,9 @@ class AC_Net(nn.Module):
 	'''
 
 	# ================================
-	def __init__(self, input_dimensions, action_dimensions, batch_size=4, hidden_types=[], hidden_dimensions=[]):
+	def __init__(self, input_dimensions, action_dimensions, 
+		batch_size=4, hidden_types=[], hidden_dimensions=[], 
+		rfsize=4, padding=1, stride=1):
 		'''
 		AC_Net(input_dimensions, action_dimensions, hidden_types=[], hidden_dimensions=[])
 
@@ -51,6 +53,7 @@ class AC_Net(nn.Module):
 			                                  used (default = []).
 			- hidden_dimensions (list of ints): the dimensions of the hidden layers. Must be a list of
 			                                    equal length to hidden_types (default = []).
+			- TODO insert new args
 		'''
        
 		# call the super-class init 
@@ -58,6 +61,11 @@ class AC_Net(nn.Module):
 
 		# store the input dimensions
 		self.input_d = input_dimensions
+		# determine input type
+		if type(input_dimensions) == int:
+			assert (hidden_types[0] != 'conv' or hidden_types[0] != 'pool')
+		elif type(input_dimensions) == tuple:
+			assert (hidden_types[0] == 'conv' or hidden_types[0] == 'pool')
 
 		# store the batch size
 		self.batch_size = batch_size
@@ -67,6 +75,7 @@ class AC_Net(nn.Module):
 		
 		# check whether we're using hidden layers
 		if not hidden_types:
+
 			self.layers = [input_dimensions,action_dimensions]
 
 			# no hidden layers, only input to output, create the actor and critic layers
@@ -75,7 +84,6 @@ class AC_Net(nn.Module):
 			self.output = nn.ModuleList([self.actor, self.critic])
 
 		else:
- 
 			# to store a record of the last hidden states
 			self.hx = []
 			self.cx = []
@@ -85,14 +93,36 @@ class AC_Net(nn.Module):
 			for i,htype in enumerate(hidden_types):
 				
 				# check that the type is an accepted one
-				assert htype in ['linear','lstm','gru']
+				assert htype in ['linear','lstm','gru', 'conv', 'pool']
 
-				# get the dimensions
+				# get the input dimensions
 				if i is 0:
 					input_d  = input_dimensions
 				else:
-					input_d  = hidden_dimensions[i-1]
-				output_d = hidden_dimensions[i]
+					if hidden_types[i-1] in ['conv','pool'] and not htype in ['conv','pool']:
+						input_d = np.prod(hidden_dimensions[i-1])
+					else:
+						input_d = hidden_dimensions[i-1]
+
+				# get the output dimensions
+				if not htype in ['conv','pool']:
+					output_d = hidden_dimensions[i]
+				elif htype in ['conv','pool']:
+					output_d = list((0,0,0))
+					if htype is 'conv':
+						output_d[0] = int(np.floor((input_d[0] + 2*padding - rfsize)/stride) + 1)
+						output_d[1] = int(np.floor((input_d[1] + 2*padding - rfsize)/stride) + 1)
+						#pdb.set_trace()
+						assert output_d[0] == hidden_dimensions[i][0]
+						assert output_d[1] == hidden_dimensions[i][1]
+						output_d[2] = hidden_dimensions[i][2]
+					elif htype is 'pool':
+						output_d[0] = int(np.floor((input_d[0] - rfsize)/stride) + 1)
+						output_d[1] = int(np.floor((input_d[1] - rfsize)/stride) + 1)
+						assert output_d[0] == hidden_dimensions[i][0]
+						assert output_d[1] == hidden_dimensions[i][1]
+						output_d[2] = hidden_dimensions[i][2]
+					output_d = tuple(output_d)
 
 				# construct the layer
 				if htype is 'linear':
@@ -101,11 +131,20 @@ class AC_Net(nn.Module):
 					self.cx.append(None)
 				elif htype is 'lstm':
 					self.hidden.append(nn.LSTMCell(input_d, output_d))
-					self.hx.append(torch.zeros(self.batch_size,output_d))
-					self.cx.append(torch.zeros(self.batch_size,output_d))
+					self.hx.append(Variable(torch.zeros(self.batch_size,output_d)))
+					self.cx.append(Variable(torch.zeros(self.batch_size,output_d)))
 				elif htype is 'gru':
 					self.hidden.append(nn.GRUCell(input_d, output_d))
-					self.hx.append(torch.zeros(self.batch_size,output_d))
+					self.hx.append(Variable(torch.zeros(self.batch_size,output_d)))
+					self.cx.append(None)
+				elif htype is 'conv':
+					#pdb.set_trace()
+					self.hidden.append(nn.Conv2d(input_d[2],output_d[2],rfsize,padding=padding,stride=stride))
+					self.hx.append(None)
+					self.cx.append(None)
+				elif htype is 'pool':
+					self.hidden.append(nn.MaxPool2d(rfsize,padding=padding,stride=stride))
+					self.hx.append(None)
 					self.cx.append(None)
 
 			# create the actor and critic layers
@@ -114,6 +153,9 @@ class AC_Net(nn.Module):
 			self.actor = nn.Linear(output_d, action_dimensions)
 			self.critic = nn.Linear(output_d, 1)
 			self.output = nn.ModuleList([self.actor, self.critic])
+
+		# store the outpud dimensions
+		self.output_d = output_d
 
 		# to store a record of actions and rewards	
 		self.saved_actions = []
@@ -138,10 +180,25 @@ class AC_Net(nn.Module):
 		'''
 
 		# check the inputs
-		assert x.shape[-1] == self.input_d
+		if type(self.input_d) == int:
+			assert x.shape[-1] == self.input_d
+		elif type(self.input_d) == tuple:
+			print( x.shape, type(self.input_d))
+			assert (x.shape[2], x.shape[3], x.shape[1]) == self.input_d
+		else:	
+		   	pdb.set_trace()
 
 		# pass the data through each hidden layer
 		for i, layer in enumerate(self.hidden):
+			# squeeze if last layer was conv/pool and this isn't
+			if i >= 0:
+				if (isinstance(self.hidden[i-1],nn.Conv2d) or isinstance(self.hidden[i-1],nn.MaxPool2d)) and \
+				not (isinstance(layer,nn.Conv2d) or isinstance(layer,nn.MaxPool2d)):
+					x = x.view(-1)
+			else:
+				print('need another case')
+
+			# run input through the layer depending on type
 			if isinstance(layer, nn.Linear):
 				x = F.relu(layer(x))
 			elif isinstance(layer, nn.LSTMCell):
@@ -151,12 +208,37 @@ class AC_Net(nn.Module):
 			elif isinstance(layer, nn.GRUCell):
 				x = layer(x, self.hx[i])
 				self.hx[i] = x.clone()
+			elif isinstance(layer, nn.Conv2d):
+				x = layer(x)
+			elif isinstance(layer, nn.MaxPool2d):
+				x = layer(x)
 
 		# pass to the output layers
 		policy = F.softmax(self.actor(x), dim=1)
 		value  = self.critic(x)
 
 		return policy, value
+
+	# ===============================
+	def reinit_hid(self):
+			# to store a record of the last hidden states
+		self.hx = []
+		self.cx = []
+	
+		for i, layer in enumerate(self.hidden):
+			if isinstance(layer, nn.Linear):
+				pass
+			elif isinstance(layer, nn.LSTMCell):
+				self.hx.append(Variable(torch.zeros(self.batch_size,layer.hidden_size)))
+				self.cx.append(Variable(torch.zeros(self.batch_size,layer.hidden_size)))
+			elif isinstance(layer, nn.GRUCell):
+				self.hx.append(Variable(torch.zeros(self.batch_size,layer.hidden_size)))
+				self.cx.append(None)
+			elif isinstance(layer, nn.Conv2d):
+				pass
+			elif isinstance(layer, nn.MaxPool2d):
+				pass
+
 
 # =====================================
 # FUNCTIONS
@@ -224,13 +306,14 @@ def select_action_end(model,policy_, value_):
 	return action.data[0], policy_.data[0], value_.data[0]
 
 
-def generate_values(maze, model,EC):
+def generate_values(maze, model,pcs,**kwargs):
 	value_map = maze.empty_map
+	EC = kwargs.get('EC', None)
 	if EC!=None:
 		EC_pol_map = maze.make_map(maze.grid, pol=True)
 		MF_pol_map = maze.make_map(maze.grid, pol=True)
 	for loc in maze.useable:
-		state = Variable(torch.FloatTensor(maze.mk_state(state=loc)))
+		state = Variable(torch.FloatTensor(pcs.activity(loc)))
 		policy, value = select_action(model,state)[1:3]
 		value_map[loc[1]][loc[0]] = value
 		if EC != None:
