602fe633d0daafb08c65449dcc940ae1c136847b
Annik Carson (drnerdbot) <annikcarson@gmail.com>
2020-04-13 13:07:47 -0400

    updating

--- notebooks/experiment.py
+++ notebooks/experiment.py
@@ -14,7 +14,28 @@ from scipy.stats import entropy
 # temp
 import matplotlib.pyplot as plt
 import time
-#/temp
+#/
+####################################################
+def get_snapshot(sample_obs, env, agent):
+    # get sample observations from all useable spaces in environment
+    samples, states = sample_obs
+
+    # forward pass through network
+    pols, vals = agent(torch.Tensor(samples))
+
+    # initialize empty data frames
+    pol_grid = np.zeros(env.shape, dtype=[('N', 'f8'), ('E', 'f8'), ('W', 'f8'), ('S', 'f8'), ('stay', 'f8'), ('poke', 'f8')])
+    val_grid = np.empty(env.shape)
+
+    # populate with data from network
+    for s, p, v in zip(states, pols, vals):
+        pol_grid[s] = tuple(p.data.numpy())
+        val_grid[s] = v.item()
+
+    return pol_grid, val_grid
+
+
+######################################################
 def run(run_dict, full=False, use_EC = False, **kwargs):
     '''
     :param run_dict: dictionary storing data frames and run parameters
@@ -206,42 +227,50 @@ def run(run_dict, full=False, use_EC = False, **kwargs):
             blocktime = time.time()
 
 
-class Experiment(object):
-    def __init__(self, environment, agent, learning_rate = 5e-4, discount_factor = 0.98, **kwargs):
 
-        self.env   = environment
+
+class Experiment(object):
+    def __init__(self, environment, agent, learning_rate=5e-4, discount_factor=0.98, **kwargs):
+        self.env = environment
         self.agent = agent
-        self.eta   = learning_rate
+        self.eta = learning_rate
         self.gamma = discount_factor
 
         # set up memory
-        self.mem_size = 0.75*environment.nstates*len(environment.action_list)
-        self.episodic = ec.ep_mem(self.agent, cache_limit=self.mem_size)
         self.record_memory = kwargs.get('rec_mem', False)
         self.use_memory = kwargs.get('use_mem', False)
-        self.mem_policy_entropy = kwargs.get('EC_entropy', 0.3)
+
+        if self.record_memory or self.use_memory:
+            self.mem_size = 0.75 * environment.nstates * len(environment.action_list)
+            self.episodic = ec.ep_mem(self.agent, cache_limit=self.mem_size)
+
+            self.key_layer = len(self.agent.hidden) - 2
+            self.mem_policy_entropy = kwargs.get('EC_entropy', 0.3)
 
         self.full = kwargs.get('full', False)
         if not self.full:
             self.trial_length = []
 
+        self.around_reward = kwargs.get('around_rwd', False)
+        self.reward_radius = kwargs.get('radius', self.env.shape[0])
+
         self.total_policy_loss = []
         self.total_value_loss = []
         self.total_reward = []
 
-        self.print_freq  = kwargs.get('print_freq', 100)
-        self.save_data   = kwargs.get('save', True)
-        self.saveplots   = kwargs.get('plots', False)
+        self.print_freq = kwargs.get('print_freq', 100)
+        self.save_data = kwargs.get('save', True)
+        self.saveplots = kwargs.get('plots', False)
 
     def reset(self, trial):
-        self.env.resetEnvironment()
+        self.env.resetEnvironment(around_rwd=self.around_reward, radius=self.reward_radius)
         self.agent.reinit_hid()
 
         if self.record_memory:
             self.episodic.reset_cache()
 
         if self.use_memory:
-                self.MF_cs = self.episodic.make_pvals(self.ploss_scale, envelope=self.mfc_env)
+            self.MF_cs = self.episodic.make_pvals(self.ploss_scale, envelope=self.mfc_env)
         else:
             self.MF_cs = 1
 
@@ -252,148 +281,115 @@ class Experiment(object):
         self.memory_buffer[0].append(timestamp)
         self.memory_buffer[1].append(lin_act)
         self.memory_buffer[2].append(choice)
-        self.memory_buffer[3].append(self.env.cur_state)
+        self.memory_buffer[3].append(current_state)
         self.memory_buffer[4] = trial
 
-    def action_selection(self, policy_, value_, lin_act):
+    def action_selection(self, policy_, value_, lin_act=None):
+        policy = policy_.data[0]
+        value = value_.item()
         if self.use_memory:
-            pol_choice = np.random.choice([0,1], p=[self.MF_cs, 1-self.MF_cs])
-            if pol_choice == 1:
-                # get policy from EC
-                episodic_memory = self.episodic.recall_mem(lin_act, self.timestamp, env=self.recency_env, mem_temp= self.mem_policy_entropy)
-                episodic_pol = torch.from_numpy(episodic_memory) ## check this env parameter
-                choice, policy, value = self.agent.select_ec_action(policy_, value_, episodic_pol)
+            pol_choice = np.random.choice(['mf', 'ec'], p=[self.MF_cs, 1 - self.MF_cs])
+            if pol_choice == 'ec':
+                episodic_memory = self.episodic.recall_mem(lin_act, self.timestamp, env=self.recency_env, mem_temp=self.mem_policy_entropy)
+                episodic_pol = torch.from_numpy(episodic_memory)
+                choice = self.agent.select_ec_action(policy_, value_, episodic_pol)
             else:
-                choice, policy, value = self.agent.select_action(policy_, value_)
+                choice = self.agent.select_action(policy_, value_)
         else:
-            choice, policy, value = self.agent.select_action(policy_, value_)
+            choice = self.agent.select_action(policy_, value_)
 
-        return choice,policy,value
+        return choice, policy, value
 
+    def forward_pass(self, state):
+        if self.record_memory or self.use_memory:
+            mf_policy, mf_value, lin_act_ = self.agent(state, lin_act=self.key_layer)
+            lin_act = tuple(np.round(lin_act_.data[0].numpy(), 4))
+        else:
+            mf_policy, mf_value = self.agent(state)
+            lin_act = None
 
-    def run(self, NUM_TRIALS, NUM_EVENTS):
-        self.ploss_scale = 0                           # this is equivalent to calculating MF_confidence = sech(0) = 1
-        self.mfc_env     = ec.calc_env(halfmax = 3.12) # 1.04 was the calculated standard deviation of policy loss after learning on open field gridworld task **** may need to change for different tasks
-        self.recency_env = ec.calc_env(halfmax = 20)
+        return mf_policy, mf_value, lin_act
 
-        reward      = 0
-        self.timestamp   = 0
+    def run(self, NUM_TRIALS, NUM_EVENTS, data_storage, **kwargs):
+        print_freq = kwargs.get('print_freq', self.print_freq)
 
-        is_done     = False
+        self.ploss_scale = 0  # this is equivalent to calculating MF_confidence = sech(0) = 1
+        self.mfc_env = ec.calc_env(halfmax=3.12)  # 1.04 was the calculated standard deviation of policy loss after learning on open field gridworld task **** may need to change for different tasks
+        self.recency_env = ec.calc_env(halfmax=20)
 
-        blocktime   = time.time()
+        reward = 0
+        self.timestamp = 0
+
+        blocktime = time.time()
+
         for trial in range(NUM_TRIALS):
-            if is_done:
-                break
-
             self.reset(trial)
 
             for event in range(NUM_EVENTS):
-                state = torch.Tensor(self.env.observation)
-                mf_policy, mf_value, lin_act_ = self.agent(state) # pass through AC network to get MF policy / value
-                lin_act = tuple(np.round(lin_act_.data[0].numpy(), 4))   # get activity of linear layer for EC dict key
+                state = torch.Tensor(np.expand_dims(self.env.observation, axis=0))
+
+                mf_policy, mf_value, lin_act = self.forward_pass(state)
 
                 choice, policy, value = self.action_selection(mf_policy, mf_value, lin_act)
-
+                action = self.env.action_list[choice][0]
                 if self.record_memory:
-                    self.save_to_mem(self.timestamp, lin_act, choice, env.oneD2twoD(env.state),trial)
+                    self.save_to_mem(self.timestamp, lin_act, choice, self.env.oneD2twoD(self.env.state), trial)
 
                 if event < NUM_EVENTS:
-                    next_state, reward, done, info = self.env.step(choice)
+                    next_state, reward, done = self.env.move(action)
 
-                self.agent.rewards.append(reward)
+                self.agent.saved_rewards.append(reward)
                 self.reward_sum += reward
                 self.timestamp += 1
 
-                if reward in self.env.rewards.values():
-                    if not self.full:
-                        self.trial_length.append(event)
-                        is_done = False
-                        break
+                if done:
+                    data_storage['trial_length'].append(event)
+                    break
 
-                if event == NUM_EVENTS-1 and not self.full:
-                    run_dict['trial_length'].append(event)
-
-            if rec_mem:
-                p_loss, v_loss = ac.finish_trial(self.agent,self.gamma,cache=self.episodic,buffer=self.memory_buffer)
+            if self.record_memory:
+                p_loss, v_loss = self.agent.finish_trial_EC(cache=self.episodic, buffer=self.memory_buffer)
             else:
-                p_loss, v_loss = ac.finish_trial(self.agent,self.gamma)
+                p_loss, v_loss = self.agent.finish_trial()
 
             self.ploss_scale = abs(p_loss.item())
-            if save_data:
-                self.total_policy_loss.append(p_loss.item())
-                self.total_value_loss.append(v_loss.item())
-                self.total_reward.append(self.reward_sum)
+            data_storage['trial_length'].append(event)
+            data_storage['total_reward'].append(self.reward_sum)
+            data_storage['loss'][0].append(p_loss.item())
+            data_storage['loss'][1].append(v_loss.item())
 
+            # if saveplots:
+            # vv, pp = ac.snapshot(agent=run_dict['agent'], maze =run_dict['environment'])
 
-            #if saveplots:
-                #vv, pp = ac.snapshot(agent=run_dict['agent'], maze =run_dict['environment'])
+            # gp.plot_polmap(run_dict['environment'], pp, save=True, show=False, title=f"{trial}")
+            # gp.plot_valmap(run_dict['environment'], vv, save=True, show=False, title=f"{trial}")
 
-                #gp.plot_polmap(run_dict['environment'], pp, save=True, show=False, title=f"{trial}")
-                #gp.plot_valmap(run_dict['environment'], vv, save=True, show=False, title=f"{trial}")
+            # abcd = ac.mem_snapshot(run_dict['environment'], agent_params['EC'], trial_timestamp=trial, decay=recency_env, mem_temp=agent_params['mem_temp'], get_vals=False)
+            # gp.plot_polmap(run_dict['environment'], abcd, threshold=0.4, save=True, show=False, title=f"EC_{trial}")
 
-                #abcd = ac.mem_snapshot(run_dict['environment'], agent_params['EC'], trial_timestamp=trial, decay=recency_env, mem_temp=agent_params['mem_temp'], get_vals=False)
-                #gp.plot_polmap(run_dict['environment'], abcd, threshold=0.4, save=True, show=False, title=f"EC_{trial}")
-
-            if trial ==0 or trial%print_freq==0 or trial == NUM_TRIALS-1:
-                print("[{0}]  Trial {1} TotRew = {2} ({3:.3f}s)".format(time.strftime("%H:%M:%S", time.localtime()), trial+1, reward_sum,time.time()-blocktime)) #print("[{0}]  Trial {1} total reward = {2} (Avg {3:.3f})".format(time.strftime("%H:%M:%S", time.localtime()), trial, reward_sum, float(reward_sum)/float(NUM_EVENTS)), "Block took {0:.3f}".format(time.time()-blocktime))
+            if trial == 0 or trial % print_freq == 0 or trial == NUM_TRIALS - 1:
+                print("[{0}]  Trial {1} TotRew = {2} ({3:.3f}s)".format(time.strftime("%H:%M:%S", time.localtime()),
+                                                                        trial + 1, self.reward_sum,
+                                                                        time.time() - blocktime))  # print("[{0}]  Trial {1} total reward = {2} (Avg {3:.3f})".format(time.strftime("%H:%M:%S", time.localtime()), trial, reward_sum, float(reward_sum)/float(NUM_EVENTS)), "Block took {0:.3f}".format(time.time()-blocktime))
                 blocktime = time.time()
 
 
 
-######################################################################3
-def get_snapshot(env, agent):
-    # get sample observations from all useable spaces in environment
-    samples, states = env.get_sample_obs()
-
-    # forward pass through network
-    pols, vals = agent(torch.Tensor(samples))
-
-    # initialize empty data frames
-    pol_grid = np.zeros(env.shape, dtype=[('N', 'f8'), ('E', 'f8'), ('W', 'f8'), ('S', 'f8'), ('stay', 'f8'), ('poke', 'f8')])
-    val_grid = np.empty(env.shape)
-
-    # populate with data from network
-    for s, p, v in zip(states, pols, vals):
-        pol_grid[s] = p.data[0].numpy()
-        val_grid[s] = v.item()
-
-    return pol_grid, val_grid
-
-def make_agent(agent_params, freeze=False):
-    if agent_params['load_model']:
-        # load previously saved model
-        MF = torch.load(agent_params['load_dir'])
-    else:
-        MF = ac.ActorCritic(agent_params)
-
-    if freeze:
-        freeze = []
-        unfreeze = []
-        for i, nums in MF.named_parameters():
-            if i[0:6] == 'output':
-                unfreeze.append(nums)
-            else:
-                freeze.append(nums)
-        MF.optimizer = optim.Adam([{'params': freeze, 'lr': 0.0}, {'params': unfreeze, 'lr': agent_params['eta']}], lr=0.0)
-    else:
-        MF.optimizer = optim.Adam(MF.parameters(), lr= agent_params['eta'])
-    return MF
-
+# =========================================================================
+'''
 rows, columns = 20, 20
 env = gw.GridWorld(rows=rows,cols=columns,
-                   rewards={(int(rows/2),int(columns/2)):1},
+                   rewards = {(5,5):1},#rewards={(int(rows/2),int(columns/2)):1},#
                    step_penalization=-0.01,
                    rho=0.0)
 agent_params = {
-    'load_model':  False,
+    'load_model':  True,
     'load_dir':     f'../data/outputs/gridworld/openfield{rows}{columns}.pt',
     'freeze_w':    False,
 
     'input_dims':  env.observation.shape,
     'action_dims': len(env.action_list),
-    'hidden_types':['conv','pool','linear'],
-    'hidden_dims': [None, None, 500],
+    'hidden_types':['conv','pool','conv','pool','linear','linear'],
+    'hidden_dims': [None, None, None, None, 500, 200],
 
     'rfsize':      5,
     'stride':      1,
@@ -408,58 +404,11 @@ agent_params = {
     'cachelim':    300,
     'mem_temp':    0.3
 }
-
-## Parameters
-NUM_TRIALS = 1000
-NUM_EVENTS = 300
-agent = ac.ActorCritic(agent_params)
-
-def run_expt(NUM_TRIALS, NUM_EVENTS, env, agent,
-               **kwargs):
-    total_reward = []
-    loss = []
-    print_freq = kwargs.get('printfreq', 0.1)
-    t = time.time()
-    for trial in range(NUM_TRIALS):
-        sar = []
-        reward_sum = 0
-        for event in range(NUM_EVENTS):
-            #get state observation
-            observation = torch.Tensor(np.expand_dims(env.get_observation(), axis=0))
-
-            #pass observation through network
-            policy_, value_ = agent(observation)
-
-            #select action from policy
-            choice = agent.select_action(policy_, value_)
-            action = env.action_list[choice][0]
-
-            #take a step in the environment
-
-            s_1d, reward, isdone = env.move(action)
-
-            agent.saved_rewards.append(reward)
-            reward_sum += reward
-            ###optional
-            #sar.append(env.state, action, reward) ## tracking oneD states
-            if isdone:
-                print(f'trial finished after {event+1} steps')
-                break
-
-        p_loss, v_loss = agent.finish_trial()
-
-        total_reward.append(reward_sum)
-        loss.append((p_loss, v_loss))
-
-        if trial % int(print_freq*NUM_TRIALS)==0:
-            print(f"{trial}: {reward_sum} ({time.time()-t}s)")
-            t = time.time()
-
-    return total_reward, loss
-
-print(int(0.1*NUM_TRIALS))
-total_reward = run_expt(NUM_TRIALS, NUM_EVENTS, env, agent)
-
-plt.plot(total_reward)
-plt.show()
-
+agent = ac.make_agent(agent_params)
+data = {'total_reward': [],
+        'loss': [[],[]],
+        'trial_length': [],
+        'trials_run_to_date':0}
+expt = Experiment(env, agent, use_ec=True)
+expt.run(NUM_TRIALS=10, NUM_EVENTS=200, data_storage=data)
+'''
