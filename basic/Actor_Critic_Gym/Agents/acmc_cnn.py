33c7da67626d22bd19794a04fafeb1cacf3bf053
kbnealy <k.b.nealy@gmail.com>
2020-10-28 21:50:32 -0400

    Added CNN + Atari functionality
    Atari games are working, however, the agent not learning...

--- basic/Actor_Critic_Gym/Agents/acmc_cnn.py
+++ basic/Actor_Critic_Gym/Agents/acmc_cnn.py
@@ -1,27 +1,29 @@
 # =====================================
-#     Creates MC Actor-Critc Agent
+#      Actor-Critc with MC + CNN 
 # =====================================
 #       Function Descriptions
 # =====================================
 # get_action_log_value = takes observation (state) and returns the action from actor network (i.e. policy), log_value from actor, and value from critic networ 
 # mc_learn = Uses stored transition from the episode to calculate losses and updates the actor and critic networks
 
-from network import Network
+from Networks.cnn import Network
 import torch as T
 import torch.nn.functional as F
-from mc_buffer import MCBuffer
+from Buffers.mc import MCBuffer
 from torch.autograd import Variable
 
-class MC_Agent():
+class Agent_cnn():
     def __init__(self, alpha, beta, input_dims, gamma=0.99, l1_size = 256, l2_size = 256, n_actions = []): #alpha/beta = agent/critic lrs
         self.log_probs = None
         self.n_actions = n_actions
         # set up AC networks and MC buffer
-        self.actor = Network(alpha, input_dims, l1_size, l2_size, n_actions)  # actor network output is action dimensions
-        self.critic = Network(beta, input_dims, l1_size, l2_size, n_actions=1)  # critic netowrk outputs is value estimate 
+        self.actor = Network(alpha, input_dims, l1_size, n_actions)  # actor network output is action dimensions
+        self.critic = Network(beta, input_dims, l1_size, n_actions=1)  # critic netowrk outputs is value estimate 
         self.MC = MCBuffer(gamma) 
     
     def get_action_log_value(self, observation):
+        # we need to add a dimension to the observation since torch expects a batch and we're feeding it a single observation
+
         # get the action produced by the actor (policy) network
         probabilities = F.softmax(self.actor.forward(observation))
         action_probs = T.distributions.Categorical(probabilities)
@@ -47,10 +49,9 @@ class MC_Agent():
             delta = target_value - observed_value.item()  # This is the delta variable (i.e target_value*self.gamma + observed_value)
             actor_loss = (-log_prob * delta)
             #print(actor_losses)
-            return_bs = Variable(T.Tensor([[target_value]])).unsqueeze(-1) # used to make the shape work out
+            #return_bs = Variable(T.Tensor([[target_value]])).unsqueeze(-1) # used to make the shape work out
             #print(observed_value.shape, return_bs.shape) # shape error - you can pass batch 
-            critic_loss = (F.smooth_l1_loss(observed_value, return_bs))
-            #critic_loss = delta**2
+            critic_loss = delta**2
             actor_losses += actor_loss
             critic_losses += critic_loss
 
