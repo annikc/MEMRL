8991a5034f8bedfa8fe633cc6e877c66845eae7f
annikc <annikcarson@gmail.com>
2021-04-02 10:16:36 -0400

    load to scinet'

--- basic/Tests/CH1/_model_free_training.py
+++ basic/Tests/CH1/_model_free_training.py
@@ -11,46 +11,44 @@ import gym
 import matplotlib.pyplot as plt
 import sys
 sys.path.append('../../modules/')
-from modules.Agents.Networks import ActorCritic as Network
+from basic.modules.Agents.Networks import ActorCritic as Network
 from basic.modules.Agents.EpisodicMemory import EpisodicMemory as Memory
-from modules.Agents import Agent
-from modules.Agents.RepresentationLearning.learned_representations import onehot, place_cell, rand_place_cell, sr
-rep_types = {'onehot': onehot, 'place_cell': place_cell, 'rand_place_cell': rand_place_cell, 'sr':sr}
+from basic.modules.Agents import Agent
+from basic.modules.Agents.RepresentationLearning.learned_representations import onehot, place_cell, rand_place_cell, sr
+rep_types = {'onehot':onehot, 'place_cell':place_cell, 'rand_place_cell':rand_place_cell, 'sr':sr}
 
-from modules.Experiments import expt
-from modules.Utils import one_hot_state
-from modules.Utils.gridworld_plotting import plot_pref_pol, plot_polmap
+from basic.modules.Experiments import expt
+from basic.modules.Utils.gridworld_plotting import plot_pref_pol, plot_polmap
 
 ## set parameters for run -- environment, representation type
 write_to_file = 'ec_training.csv'
 env_name = 'gridworld:gridworld-v1'
 representation_type = 'onehot'
-num_trials = 1000
+num_trials = 5000
 num_events = 250
 
 
+for _ in range(5):
+    ## generate the environment object
+    env = gym.make(env_name)
+    plt.close()
 
-## generate the environment object
-env = gym.make(env_name)
-plt.close()
+    ## get state representations to be used
+    state_reps, representation_type, input_dims = rep_types[representation_type](env)
 
-## get state representations to be used
-state_reps, representation_type, input_dims = rep_types[representation_type](env, field_size=0.1)
+    ## create an actor-critic network and associated agent
+    network = Network(input_dims=[input_dims], fc1_dims=200, fc2_dims=200, output_dims=env.action_space.n, lr=0.0005)
+    memory = Memory(entry_size=env.action_space.n, cache_limit=400, mem_temp=1)
+    agent = Agent(network, state_representations=state_reps, memory=memory)
 
-## create an actor-critic network and associated agent
-network = Network(input_dims=[input_dims],fc1_dims=200,fc2_dims=200,output_dims=env.action_space.n, lr=0.0005)
-memory = Memory(entry_size=env.action_space.n, cache_limit=400, mem_temp=1)
-agent = Agent(network, state_representations=state_reps, memory=memory)
+    # create an experiment class instance
+    ex = expt(agent, env)
 
-# create an experiment class instance
-ex = expt(agent,env)
+    ex.run(num_trials, num_events)
 
-ex.run(num_trials, num_events)
-ex.log_items()
-
-ex.record_log(env_name=env_name,representation_type=representation_type,
-              n_trials=num_trials, n_steps=num_events,
-              dir='../../Data/', file=write_to_file)
+    ex.record_log(env_name=env_name, representation_type=representation_type,
+                  n_trials=num_trials, n_steps=num_events,
+                  dir='../../Data/', file=write_to_file)
 
 # print results of training
 fig, ax = plt.subplots(2,1, sharex=True)
@@ -67,6 +65,6 @@ for state_i in range(env.nstates):
     h = ex.agent.EC.recall_mem(state_obs)
     ep_pols[env.oneD2twoD(state_i)] = tuple(h)
 
-plot_polmap(env, ep_pols)
+plot_pref_pol(env, ep_pols)
 
 plt.show()
