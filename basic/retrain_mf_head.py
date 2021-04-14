873d31f7742c1977cae7f500543e2fe51bbf9954
AnnikSciNet <annik.yalnizyan.carson@mail.utoronto.ca>
2021-04-14 16:08:30 -0400

    pulling mf head training without load weights

--- basic/retrain_mf_head.py
+++ basic/retrain_mf_head.py
@@ -8,17 +8,19 @@ sys.path.append('../../modules')
 from modules.Agents.Networks import flat_ActorCritic as head_AC
 from modules.Agents.Networks import flex_ActorCritic as Network
 from modules.Agents.Networks.load_network_weights import load_saved_head_weights, convert_agent_to_weight_dict
-from modules.Agents.RepresentationLearning.learned_representations import saved_latents
+from modules.Agents.RepresentationLearning.learned_representations import latents
 from modules.Agents import Agent
 from modules.Experiments import flat_expt
 sys.path.append('../../../')
 
 
 write_to_file = 'head_only_retrain.csv'
-version = 1
+version = 4
 latent_type = 'conv'
 training_env_name = f'gridworld:gridworld-v{version}'
 test_env_name = training_env_name+'1'
+
+relative_path_to_data = './Data/' # from within Tests/CH1
 
 num_trials = 25000
 num_events = 250
@@ -33,6 +35,7 @@ conv_ids = {'gridworld:gridworld-v1':'c34544ac-45ed-492c-b2eb-4431b403a3a8',
            'gridworld:gridworld-v4':'b50926a2-0186-4bb9-81ec-77063cac6861',
            'gridworld:gridworld-v5':'15b5e27b-444f-4fc8-bf25-5b7807df4c7f'}
 ids = {'conv':conv_ids, 'rwd_conv':rwd_conv_ids}
+
 id_dict = ids[latent_type]
 run_id = id_dict[training_env_name]
 
@@ -43,19 +46,15 @@ plt.close()
 test_env = gym.make(test_env_name)
 plt.close()
 
-
 # load latent states to use as state representations to actor-critic heads
-agent_path = f'./Data/agents/{run_id}.pt'
-state_reps, representation_name, input_dims, _ = saved_latents(train_env,type=latent_type)
-print(state_reps[0])
-#convert_agent_to_weight_dict(agent_path)
+agent_path = relative_path_to_data+f'agents/{run_id}.pt'
+
+# save latents by loading network, passing appropriate tensor, getting top fc layer activity
+state_reps, representation_name, input_dims, _ = latents(train_env, agent_path, type=latent_type)
+
 # load weights to head_ac network from previously learned agent
 empty_net = head_AC(input_dims, test_env.action_space.n, lr=0.0005)
-print('init weight',empty_net.pol.weight)
 AC_head_agent = load_saved_head_weights(empty_net, agent_path)
-print('load weight',AC_head_agent.pol.weight)
-print(AC_head_agent)
-
 
 
 agent = Agent(AC_head_agent, state_representations=state_reps)
@@ -63,5 +62,6 @@ agent = Agent(AC_head_agent, state_representations=state_reps)
 ex = flat_expt(agent, test_env)
 ex.run(num_trials,num_events,snapshot_logging=False)
 ex.record_log(env_name=test_env_name, representation_type=representation_name,
-              n_trials=num_trials, n_steps=num_events,
-              dir='./Data/', file=write_to_file)
+              n_trials=num_trials, n_steps=num_events,load_from=run_id,
+              dir=relative_path_to_data, file=write_to_file)
+
