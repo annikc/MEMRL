62446cfe573ee22fbed2031a7ed401d414fdafeb
AnnikSciNet <annik.yalnizyan.carson@mail.utoronto.ca>
2021-04-14 10:34:52 -0400

    save before pull

--- basic/retrain_mf_head.py
+++ basic/retrain_mf_head.py
@@ -22,10 +22,11 @@ test_env_name = training_env_name+'1'
 num_trials = 25000
 num_events = 250
 
-id_dict = {'gridworld:gridworld-v1':'c34544ac-45ed-492c-b2eb-4431b403a3a8',
-           'gridworld:gridworld-v3':'32301262-cd74-4116-b776-57354831c484',
-           'gridworld:gridworld-v4':'b50926a2-0186-4bb9-81ec-77063cac6861',
-           'gridworld:gridworld-v5':'15b5e27b-444f-4fc8-bf25-5b7807df4c7f'}
+id_dict = {'gridworld:gridworld-v1':'990b45e3-49a6-49e0-8b85-e1dbbd865504',
+           'gridworld:gridworld-v3':'4ebe79ad-c5e6-417c-8823-a5fceb65b4e0',
+           'gridworld:gridworld-v4':'062f76a0-ce05-4cce-879e-2c3e7d00d543',
+           'gridworld:gridworld-v5':'fee85163-212e-4010-b90a-580e6671a454'}
+
 run_id = id_dict[training_env_name]
 
 # make gym environment to load states for getting latent representations
@@ -39,7 +40,7 @@ plt.close()
 # load latent states to use as state representations to actor-critic heads
 agent_path = f'./Data/agents/{run_id}.pt'
 state_reps, representation_name, input_dims, _ = latents(train_env,agent_path)
-convert_agent_to_weight_dict(agent_path)
+#convert_agent_to_weight_dict(agent_path)
 # load weights to head_ac network from previously learned agent
 empty_net = head_AC(input_dims, test_env.action_space.n, lr=0.0005)
 AC_head_agent = load_saved_head_weights(empty_net, agent_path)
@@ -50,6 +51,6 @@ agent = Agent(AC_head_agent, state_representations=state_reps)
 
 ex = flat_expt(agent, test_env)
 ex.run(num_trials,num_events,snapshot_logging=False)
-ex.record_log(env_name=env_name, representation_type=representation_name,
+ex.record_log(env_name=test_env_name, representation_type=representation_name,
               n_trials=num_trials, n_steps=num_events,
               dir='./Data/', file=write_to_file)
