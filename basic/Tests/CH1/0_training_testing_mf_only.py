d4dc257fb20b4373382857cc48fcf669c88b2227
annikc <annikcarson@gmail.com>
2021-08-04 21:46:12 -0400

    update data directory in MF train test

--- basic/Tests/CH1/0_training_testing_mf_only.py
+++ basic/Tests/CH1/0_training_testing_mf_only.py
@@ -30,7 +30,7 @@ write_to_file = 'naive_mf.csv'
 directory = '.Data/' # ../../Data if you are in Tests/CH2
 env_name = f'gridworld:gridworld-v{version}'
 
-num_trials = 50
+num_trials = 5000
 num_events = 250
 
 # make gym environment
@@ -60,7 +60,7 @@ test_run = expt(agent, test_env)
 test_run.data = run.data
 test_run.run(NUM_TRIALS=num_trials*2,NUM_EVENTS=num_events)
 
-test_run.record_log(env_name, representation_name,num_trials*3,num_events,dir=directory, file=write_to_file,mock_log=True)
+test_run.record_log(env_name, representation_name,num_trials*3,num_events,dir=directory, file=write_to_file)
 
 '''
 smoothing=10
